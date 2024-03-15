0684e8c0cae8aa0adee18ec86843026418fd7e88
Abused957 <abused1988@gmail.com>
2024-03-15 17:06:48 +0100

    dia dos con angular

--- angular/Company/src/app/app.component.ts
+++ angular/Company/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EmployeesComponent, EmployeeComponent],
+  imports: [RouterOutlet, EmployeesComponent, EmployeeComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- angular/Company/src/app/employee/employee.component.ts
+++ angular/Company/src/app/employee/employee.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   
   
   templateUrl: './employee.component.html',
@@ -14,5 +18,58 @@ import { Component } from '@angular/core';
   //styles:["p{max-width: 100%; color: red; margin: 0 auto; text-align: center;}"]
 })
 export class EmployeeComponent {
+
+  nombre:string = "Juan";
+  apellido:string = "Díaz";
+  private edad: number = 8;
+  empresa: string = "Decroly S.A."
+  form: FormGroup;
+
+  disableProperty = true;
+
+  userRegister = false;
+
+  constructor(private fb: FormBuilder)
+  {
+    this.form = this.fb.group
+    (
+      {
+        nuevaEmpresa:[''],
+        nuevoApellido:['']
+      }
+    )
+  }
+
+  getEdad()
+  {
+    return this.edad;
+  }
+  changeEmployeeForm()
+  {
+    const nuevaEmpresa = this.form.get('nuevaEmpresa')!.value;
+
+    const nuevoApellido = this.form.get('nuevoApellido')!.value;
+
+   if (nuevaEmpresa !== '') {
+    this.empresa = nuevaEmpresa;
+  } 
+   if (nuevoApellido !== '') {
+    this.apellido = nuevoApellido;
+  }
+
+  //limpiar los fields
+
+  this.form.get('nuevaEmpresa')!.setValue('');
+  this.form.get('nuevoApellido')!.setValue('');
+
+  }
+
+ 
+  getUserRegister()
+  {
+    this.userRegister = true;
+  }
+
+
 
 }
